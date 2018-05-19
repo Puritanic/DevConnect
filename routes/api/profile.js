ed80e70b083f8fe3fa998fb27fa0291594595595
Puritanic <darktasevski@gmail.com>
2018-05-19 18:18:24 +0200

    Add more Profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -40,6 +40,70 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 });
 
 /**
+ * @route   GET api/profile/all
+ * @desc    GEt all profiles
+ * @access  Public
+ */
+router.get('/all', (req, res) => {
+	const errors = {};
+	Profile.find()
+		.populate('user', ['name', 'avatar']) // Populate with data from User Scheme that's connected through Profile Scheme
+		.then(profiles => {
+			if (!profiles) {
+				errors.noProfile = 'There are no profiles, or something bad just happened :(';
+				return res.status(404).json(errors);
+			}
+
+			res.json(profiles);
+		})
+		.catch(() =>
+			res
+				.json(404)
+				.json({ profile: 'There are no profiles, or something bad just happened :(' })
+		);
+});
+
+/**
+ * @route   GET api/profile/handle/:handle
+ * @desc    GEt profile by handle
+ * @access  Public
+ */
+router.get('/handle/:handle', (req, res) => {
+	const errors = {};
+	Profile.findOne({ handle: req.params.handle })
+		.populate('user', ['name', 'avatar']) // Populate with data from User Scheme that's connected through Profile Scheme
+		.then(profile => {
+			if (!profile) {
+				errors.noProfile = 'There is no profile page for this user';
+				return res.status(404).json(errors);
+			}
+
+			res.json(profile);
+		})
+		.catch(err => res.json(404).json(err));
+});
+
+/**
+ * @route   GET api/profile/user/:user_id
+ * @desc    GEt profile by user id
+ * @access  Public
+ */
+router.get('/user/:user_id', (req, res) => {
+	const errors = {};
+	Profile.findOne({ user: req.params.user_id })
+		.populate('user', ['name', 'avatar']) // Populate with data from User Scheme that's connected through Profile Scheme
+		.then(profile => {
+			if (!profile) {
+				errors.noProfile = 'There is no profile page for this user';
+				return res.status(404).json(errors);
+			}
+
+			res.json(profile);
+		})
+		.catch(() => res.json(404).json({ profile: 'There is no profile page here' }));
+});
+
+/**
  * @route   Post api/profile
  * @desc    Create or update user profile
  * @access  Private
